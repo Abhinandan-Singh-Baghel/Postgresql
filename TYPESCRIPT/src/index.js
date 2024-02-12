944a94dcbe98a7d2fcd7727a42b2899e2bb2aadf
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-02-13 01:13:14 +0530

    Inserting data to database

---
+++ TYPESCRIPT/src/index.js
@@ -0,0 +1,37 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = __importDefault(require("pg"));
+const { Client } = pg_1.default;
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+function insertData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const client = new Client({
+            connectionString: "postgresql://abhinandansinghbaghel2001:" + PGPASSWORD + "@ep-muddy-mouse-a5fo427v.us-east-2.aws.neon.tech/Kakarot?sslmode=require"
+        });
+        try {
+            yield client.connect();
+            const insertQuery = "INSERT INTO users (username, email, password) VALUES ('Abhinandan', 'asbaghel@gmail.com', 'user_password');";
+            const res = yield client.query(insertQuery);
+            console.log('Insertion Success: ', res); // Output the insertion result
+        }
+        catch (err) {
+            console.error('Error during the insertion: ', err);
+        }
+        finally {
+            yield client.end(); // Close the client connection
+        }
+    });
+}
+insertData();
--- TYPESCRIPT/src/index.ts
+++ TYPESCRIPT/src/index.ts
@@ -6,9 +6,26 @@ const { Client } = pkg;
 let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 
+async function insertData() {
 
+    
 const client = new Client({
     connectionString: "postgresql://abhinandansinghbaghel2001:"+PGPASSWORD+"@ep-muddy-mouse-a5fo427v.us-east-2.aws.neon.tech/Kakarot?sslmode=require"
   })
 
-  
+
+  try {
+    await client.connect();  
+    const insertQuery = "INSERT INTO users (username, email, password) VALUES ('Abhinandan', 'asbaghel@gmail.com', 'user_password');";
+    const res = await client.query(insertQuery);
+    console.log('Insertion Success: ', res); // Output the insertion result
+
+  } catch (err){
+    console.error('Error during the insertion: ', err);
+  } finally {
+    await client.end();  // Close the client connection
+  }
+    
+}
+
+insertData();
